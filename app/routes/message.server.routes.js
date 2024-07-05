edd5e9518ee431a7549f552eb7128733dd1e8249
Michael Igbo <igboma.michael@gmail.com>
2024-07-05 13:00:58 -0700

    update

--- app/routes/message.server.routes.js
+++ app/routes/message.server.routes.js
@@ -1,12 +1,10 @@
-'use strict';
+"use strict";
 
-let express = require('express')
-let router = express.Router()
+let express = require("express");
+let router = express.Router();
 
-const message = require('../controllers/message.server.controller');
-router.get('/messages', message.getMessages)
-router.post('/messages/', message.postMessage)
-router.get('/messages/:id', message.getSingleMessage)
-router.delete('/messages/:id', message.deleteMessage)
-
-module.exports = router
+const message = require("../controllers/message.server.controller");
+router.get("/messages", message.getMessages);
+router.post("/messages/", message.postMessage);
+router.get("/messages/:id", message.getSingleMessage);
+router.delete("/messages/:id", message.deleteMessage);
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const config = require('./config/config')
+const config = require("./config/config");
 
 // Init the express application
-const app = require('./config/express')()
+const app = require("./config/express")();
 
 // Start the app by listening on <port>
-app.listen(config.port)
+app.listen(config.port);
 
-process.on('SIGINT', function () {
-    console.log("Gracefully shutting down from SIGINT (Ctrl-C)")
-    // some other closing procedures go here
-    process.exit(1)
-})
+process.on("SIGINT", function () {
+  console.log("Gracefully shutting down from SIGINT (Ctrl-C)");
+  // some other closing procedures go here
+  process.exit(1);
+});
 
 // Expose app
-exports = module.exports = app
+exports = module.exports = app;
 
 // Logging initialization
-console.log('Starting server on port 8080')
+console.log("Starting server on port ", config.port);
